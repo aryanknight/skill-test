0acbb0c12b44925c155cc4e96dd62d58112a4dc9
Aryan Knight <aryan313122@gmail.com>
2022-01-03 13:19:11 +0530

    anim added

--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -28,30 +28,33 @@ export default function UserCard({gender,name,username,email,dob,address,phone,i
 
     return (
         <div className='user-card-cont' data-aos="flip-up" data-aos-duration="1000">
-            <div className="user-img-cont">
-                <div className="user-img-circle">
-                    <img src={img} alt="" className="user-img" />
+            <div className="anim-box"></div>
+            <div className="user-card-cont-1">
+                <div className="user-img-cont">
+                    <div className="user-img-circle">
+                        <img src={img} alt="" className="user-img" />
+                    </div>
                 </div>
-            </div>
-            <div className="user-btn-cont">
-                {btnState ? (<div className="user-btn" onClick={(e) =>{expand(username)}}>
-                    More
-                </div>):
-                (<div className="user-btn" onClick={(e) =>{hide(username)}}>
-                    Less
-                </div>)}
-            </div>
-            <div className="user-details-cont">
-                <div className="user-full-name">{name}</div>
-                <div className="user-username">{username}</div>
-                
-            </div>
-            <div className="extra-details" id={username}>
-                <div className="user-gender"><b>Gender :</b> {gender}</div>
-                <div className="user-email"><b>Email :</b> {email}</div>
-                <div className="user-dob"><b>DOB :</b> {DOB}</div>
-                <div className="user-address"><b>Address :</b> {address}</div>
-                <div className="user-ph-no"><b>Phone No :</b> {phone}</div>
+                <div className="user-btn-cont">
+                    {btnState ? (<div className="user-btn" onClick={(e) =>{expand(username)}}>
+                        More
+                    </div>):
+                    (<div className="user-btn" onClick={(e) =>{hide(username)}}>
+                        Less
+                    </div>)}
+                </div>
+                <div className="user-details-cont">
+                    <div className="user-full-name">{name}</div>
+                    <div className="user-username">{username}</div>
+                    
+                </div>
+                <div className="extra-details" id={username}>
+                    <div className="user-gender"><b>Gender :</b> {gender}</div>
+                    <div className="user-email"><b>Email :</b> {email}</div>
+                    <div className="user-dob"><b>DOB :</b> {DOB}</div>
+                    <div className="user-address"><b>Address :</b> {address}</div>
+                    <div className="user-ph-no"><b>Phone No :</b> {phone}</div>
+                </div>
             </div>
         </div>
     )
